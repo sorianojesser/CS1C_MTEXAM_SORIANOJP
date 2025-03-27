9f7da7bcb3d8849f68d7678ee7cfa9f03af0ec1b
sorianojesser <jessersoriano49@gmail.com>
2025-03-27 10:38:44 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,7 @@
+let subjectTitle = prompt("Enter the subject title: ");
+let classSchedule = prompt("Enter the class schedule: ");
+let classroom = prompt("Enter the classroom: ");
+let classInstructor = prompt("Enter the class instructor": ");
+let studentName = prompt("Enter your name: ");
+
+console.log("${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.");
