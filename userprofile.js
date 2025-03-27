e7ca7d997f39e2f3791ec9dfb834c26c5402bfd2
sorianojesser <jessersoriano49@gmail.com>
2025-03-27 10:24:02 +0800

    Update and rename README.md to userprofile.js

---
+++ userprofile.js
@@ -0,0 +1,12 @@
+//User Profile
+
+let fullname = prompt("Enter your full name: ");
+let age = prompt("Enter your age: ");
+let favoritenumber = prompt("Enter you favorite number: ");
+let favoritecolor = prompt("Enter your favorite color: ");
+
+console.log("User Information: ");
+console.log("Full Name: ", fullname);
+console.log("Age: ", age);
+console.log("Favorite Number: ", favoritenumber);
+console.log("Favorite Color: ", favoritecolor);
